5592c2bb38f06ebc5e7858ca73f2f01bfdd88741
rvfch <fenix25509@gmail.com>
2018-11-13 20:16:01 +0100

    fix travis again

--- config/database.php
+++ config/database.php
@@ -56,12 +56,12 @@ return [
 
         'testing' => [
             'driver' => 'mysql',
-            'host' => env('DB_TEST_HOST', 'localhost'),
+            'host' => env('DB_TEST_HOST', '127.0.0.1'),
             'database' => env('DB_TEST_DATABASE', 'homestead_test'),
-            'username' => env('DB_USERNAME', 'homestead'),
-            'password' => env('DB_PASSWORD', 'secret'),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'strict' => false,
         ],
